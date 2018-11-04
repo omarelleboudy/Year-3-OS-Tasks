5a9f8310caa76f18a382802af1b300b2a0471a9d
omarelleboudy <omarelleboudy@gmail.com>
2018-11-04 16:39:29 +0200

    Update Matrix Sum With Multi-threading.c

--- Matrix Sum With Multi-threading.c
+++ Matrix Sum With Multi-threading.c
@@ -7,17 +7,12 @@
 #define M 4 
 
 pthread_t threads[N]; 
-int A[M][M] = { {1,2,3,4}, {1,2,3,4}, {1,2,3,4}, {1,2,3,4} },
-    B[M][M] = { {4,3,2,1}, {4,3,2,1}, {4,3,2,1}, {4,3,2,1} }, 
+int A[M][M] = { {1,4,3,5}, {1,12,3,32}, {4,2,4,4}, {1,2,3,4} },
+    B[M][M] = { {14,53,42,1}, {4,3,334,1}, {14,23,23,41}, {34,45,58,61} }, 
     sum[M][M]; 
 
-void* Add(void* arg) 
-{ 
-	int i, j; 
-	for (i = 0; i < M; i++)
-		for (j = 0; j < M; j++) 
-			sum[i][j] = A[i][j] + B[i][j]; 
-} 
+void* Sum(void* arg);
+
 
 
 int main() 
@@ -25,7 +20,7 @@ int main()
     int i, j, it = 0; 
 
     for(i = 0; i < N; i++)
-      pthread_create(&threads[i], NULL, &Add, (void*)it++);
+      pthread_create(&threads[i], NULL, &Sum, (void*)it++);
       
   
     for(i = 0; i < N ; i++) 
@@ -43,3 +38,12 @@ int main()
 
     return 0; 
 } 
+
+void* Sum(void* arg) 
+{ 
+	int i, j; 
+	for (i = 0; i < M; i++)
+		for (j = 0; j < M; j++) 
+			sum[i][j] = A[i][j] + B[i][j]; 
+} 
+
